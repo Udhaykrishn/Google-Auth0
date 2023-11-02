f43f1206e10932886655f96fe5db4562e80b7c4b
Udhaykrishn <krishnauday320@gmail.com>
2023-11-02 17:39:17 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+);
